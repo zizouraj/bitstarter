b2761a9f4795fe111f6d1fb6db7f66c8732b5571
zizouraj <zizou.spring@gmail.com>
2013-07-19 00:09:37 +0000

    Reading Data from Index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
+
+var mssg = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(mssg);
 });
 
 var port = process.env.PORT || 5000;
